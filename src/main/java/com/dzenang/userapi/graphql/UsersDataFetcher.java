b42224fb49fe6bb6e4080e8bf5f9af645a6d8106
dzenang <dzenan.granulo@gmail.com>
2024-08-29 14:07:29 +0200

    Add user query

--- src/main/java/com/dzenang/userapi/graphql/UsersDataFetcher.java
+++ src/main/java/com/dzenang/userapi/graphql/UsersDataFetcher.java
@@ -3,6 +3,7 @@ package com.dzenang.userapi.graphql;
 import com.dzenang.userapi.model.User;
 import com.dzenang.userapi.repository.UserRepository;
 import com.netflix.graphql.dgs.DgsComponent;
+import com.netflix.graphql.dgs.DgsMutation;
 import com.netflix.graphql.dgs.DgsQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -20,5 +21,10 @@ public class UsersDataFetcher {
     @DgsQuery
     public List<User> users() {
         return (List<User>) userRepository.findAll();
+    }
+
+    @DgsQuery
+    public User user(Long id) {
+        return userRepository.findById(id).orElse(null);
     }
 }
